a30f5271737a43defb9b70050f00da32715fdc94
felipesouza <felipe.macieldsouza@gmail.com>
2024-04-17 16:38:35 -0300

    init

---
+++ sag/src/test/kotlin/br/com/sag/Startup.kt
@@ -0,0 +1,13 @@
+package br.com.sag
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StartuptionTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
